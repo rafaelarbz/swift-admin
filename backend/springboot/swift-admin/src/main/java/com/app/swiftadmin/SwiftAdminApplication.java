007d3612369f999c536b6ed04a3f1aad364bbf4a
rafaelarbz <rafaelarabeloeng@gmail.com>
2023-11-28 20:09:32 -0300

    backend first commit

---
+++ backend/springboot/swift-admin/src/main/java/com/app/swiftadmin/SwiftAdminApplication.java
@@ -0,0 +1,13 @@
+package com.app.swiftadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftAdminApplication.class, args);
+	}
+
+}
